325c5ec51b2ce43ee3c229eac53fb9c5a77e5302
ner0x652 <cornel.punga@gmail.com>
2017-07-25 00:41:18 +0300

    [Elfie] Added section and segment properties

--- elfie.py
+++ elfie.py
@@ -5,16 +5,16 @@ import sys
 import termcolor as tc
 
 
+def get_typeval_as_str(lief_type):
+    return str(lief_type).split('.')[1]
+
+
 def show_name(binary):
     print(tc.colored("[::] Name", "blue"))
     print(binary.name)
 
 
 def enum_header(header):
-    def get_typeval_as_str(lief_type):
-        return str(lief_type).split('.')[1]
-
-
     def get_ident_props():
         identity  = "\n{0:18} {1}".format("\t\tClass:",   get_typeval_as_str(header.identity_class))
         identity += "\n{0:18} {1}".format("\t\tData:",    get_typeval_as_str(header.identity_data))
@@ -26,19 +26,19 @@ def enum_header(header):
 
 
     print(tc.colored("[::] Header", "blue"))
-    print(tc.colored("{0:25} {1}".format("\tEntrypoint:",             hex(header.entrypoint)), "green"))
-    print(tc.colored("{0:25} {1}".format("\tFile type:",              get_typeval_as_str(header.file_type)), "green"))
-    print(tc.colored("{0:25} {1}".format("\tHeader size:",            hex(header.header_size)), "green"))
-    print(tc.colored("{0:25} {1}".format("\tIdentity:",               get_ident_props()), "cyan"))
-    print(tc.colored("{0:25} {1}".format("\tNumber of sections:",     header.numberof_sections), "green"))
-    print(tc.colored("{0:25} {1}".format("\tNumber of segments:",     header.numberof_segments), "green"))
+    print(tc.colored("{0:25} {1}".format("\tEntrypoint:",             hex(header.entrypoint)),                         "green"))
+    print(tc.colored("{0:25} {1}".format("\tFile type:",              get_typeval_as_str(header.file_type)),           "green"))
+    print(tc.colored("{0:25} {1}".format("\tHeader size:",            hex(header.header_size)),                        "green"))
+    print(tc.colored("{0:25} {1}".format("\tIdentity:",               get_ident_props()),                              "cyan"))
+    print(tc.colored("{0:25} {1}".format("\tNumber of sections:",     header.numberof_sections),                       "green"))
+    print(tc.colored("{0:25} {1}".format("\tNumber of segments:",     header.numberof_segments),                       "green"))
     print(tc.colored("{0:25} {1}".format("\tObject file version:",    get_typeval_as_str(header.object_file_version)), "green"))
-    print(tc.colored("{0:25} {1}".format("\tProcessor flag:",         header.processor_flag), "green"))
-    print(tc.colored("{0:25} {1}".format("\tProgram header offset:",  hex(header.program_header_offset)), "green"))
-    print(tc.colored("{0:25} {1}".format("\tProgram header size:",    hex(header.program_header_size)), "green"))
-    print(tc.colored("{0:25} {1}".format("\tSection header offset:",  hex(header.section_header_offset)), "green"))
-    print(tc.colored("{0:25} {1}".format("\tSection name table idx:", hex(header.section_name_table_idx)), "green"))
-    print(tc.colored("{0:25} {1}".format("\tSection header size:",    hex(header.sizeof_section_header)), "green"))
+    print(tc.colored("{0:25} {1}".format("\tProcessor flag:",         header.processor_flag),                          "green"))
+    print(tc.colored("{0:25} {1}".format("\tProgram header offset:",  hex(header.program_header_offset)),              "green"))
+    print(tc.colored("{0:25} {1}".format("\tProgram header size:",    hex(header.program_header_size)),                "green"))
+    print(tc.colored("{0:25} {1}".format("\tSection header offset:",  hex(header.section_header_offset)),              "green"))
+    print(tc.colored("{0:25} {1}".format("\tSection name table idx:", hex(header.section_name_table_idx)),             "green"))
+    print(tc.colored("{0:25} {1}".format("\tSection header size:",    hex(header.sizeof_section_header)),              "green"))
     
 
 def show_interpreter(binary):
@@ -104,13 +104,32 @@ def enum_sections(binary):
     print(tc.colored("[::] Sections", "blue"))
     for s in binary.sections:
         print(s)
-    
+        # Properties
+        print(tc.colored("\t{0:15} {1}".format("Alignment",     hex(s.alignment)),           "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Entropy",       s.entropy),                  "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("File offset",   hex(s.file_offset)),         "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Flags",         s.flags),                    "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Information",   s.information),              "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Link",          s.link),                     "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Name index",    s.name_idx),                 "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Offset",        hex(s.offset)),              "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Original size", s.original_size),            "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Size",          s.size),                     "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Type",          get_typeval_as_str(s.type)), "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Virtual addr",  hex(s.virtual_address)),     "cyan"))
+
 
 def enum_segments(binary):
     print(tc.colored("[::] Segments", "blue"))
     for s in binary.segments:
         print(s)
-
+        # Properties
+        print(tc.colored("\t{0:15} {1}".format("Alignment",     hex(s.alignment)),           "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("File offset",   hex(s.file_offset)),         "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Flags",         s.flag),                     "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Type",          get_typeval_as_str(s.type)), "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Virtual addr",  hex(s.virtual_address)),     "cyan"))
+        print(tc.colored("\t{0:15} {1}".format("Virtual size",  hex(s.virtual_size)),        "cyan"))
 
 def run():
     if len(sys.argv) < 2:
